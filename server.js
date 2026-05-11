66283b50c76682bee5e2373a24ee06e80ce6ec49
Arianna <heartmend4u@gmail.com>
2026-05-11 15:56:54 -0600

    fix: smtp puerto 587

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(express.json());
 const pendingUsers = {};
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
